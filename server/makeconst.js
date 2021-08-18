2be9c7bd919feb8647d70a540eb42d299b6d8769
gaoyuhao <gaoyh1991@gmail.com>
2021-08-18 19:51:06 +0800

    add 图片

--- server/makeconst.js
+++ server/makeconst.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const _ = require('lodash')
-const http = require('http')
+let request = require('request')
+let path = require('path')
+
 
 const base = (name)=> `https://info.support.huawei.com/network/imagelib/downloadZip4web?productName=${name}&imgType=pic%2524%2524${name}_pic.zip&domain=0&lang=zh&ipNum=local`
 
@@ -20,12 +22,32 @@ fs.readFile('./final.json',{encoding:'utf-8'},(err,data)=>{
     })
     newdata.map(item=>{
         if(item){
-
+            downloadZIP(item)
         }
     })
+    
     console.log(newdata,newdata.length)
 });
 
+//文件下载
 
 
+//创建文件夹目录
+const dirPath = path.join(__dirname, "file");
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+    console.log("文件夹创建成功");
+} else {
+    console.log("文件夹已存在");
+}
+
+//循环多线程下载
+function downloadZIP(name){
+    let fileName = name+'.zip';
+    let url = base(name)
+    let stream = fs.createWriteStream(path.join(dirPath, fileName));
+    request(url).pipe(stream).on("close", function (err) {
+        console.log("文件[" + fileName + "]下载完毕");
+    });
+}
 
