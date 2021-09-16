6a53666657201661e7c698320267e476d20dfa0c
Gss615 <gao.yh1991@gmail.com>
2021-09-16 09:32:41 +0800

    删除不必须要的文字

--- src/components/Header.js
+++ src/components/Header.js
@@ -69,9 +69,6 @@ export default class Header extends React.Component {
                                 </div>
                                 <button className="link snipcart-checkout hamburger__checkout">
                                     在线询价
-                                    <svg width="26" height="14" viewBox="0 0 26 14" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M22.682 6.079h-22.682v1.712h22.814l-4.574 4.528 1.194 1.182 6.566-6.5-6.566-6.5-1.194 1.182 4.442 4.397z" />
-                                    </svg>
                                 </button>
                                 <ul className="hamburger__nav">
                                     {_.map(_.get(site, 'siteMetadata.main_menu', null), (item, item_idx) => {
